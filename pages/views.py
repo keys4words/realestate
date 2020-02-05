961275856ab12605676f88e6595a48412e86391f
keys4words <keys4words@gmail.com>
2020-02-05 14:37:01 +0300

    add base.html to templates

--- pages/views.py
+++ pages/views.py
@@ -4,4 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse('<h1>Prove it!</h1>')
+    return render(request, 'pages/index.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
